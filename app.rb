b3c883f593800e03af3c7d2864434e0d73973e77
sophie <justanothersophie@gmail.com>
2021-08-24 15:36:06 -0400

    Completed lab

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Sophia"
+    end
+
+    get '/hometown' do
+        "My hometown is West Chester"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is A Better Son/Daughter"
+    end
 end
